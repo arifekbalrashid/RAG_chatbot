3ca6f7f72edcbbba961d3a4b8fcbfea2b110dc15
Md Arif Ekbal Rashid <arifekbalrashid786@gmail.com>
2026-03-18 02:14:07 +0530

    Fix start script for Render+local compatibility

--- start.sh
+++ start.sh
@@ -1,1 +1,8 @@
-streamlit run app.py --server.port $PORT --server.address 0.0.0.0
+#!/bin/bash
+
+PORT_TO_USE=${PORT:-8501}
+
+streamlit run app.py \
+  --server.port=$PORT_TO_USE \
+  --server.address=0.0.0.0 \
+  --server.headless=true
